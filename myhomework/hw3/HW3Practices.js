4da235d86d28dc36e75b26cbdab1e5231c4319a1
Jason Chay <Jason Chay>
2016-12-10 10:35:47 -0500

    Renamed HW3Practices to have .js extension and updated min function

--- myhomework/hw3/HW3Practices.js
+++ myhomework/hw3/HW3Practices.js
@@ -2,7 +2,12 @@ function product(a,b){
   return a*b;
 }
 function min(a, b){
-  console.log(Math.min(a, b))
+  if (a < b){
+    return a;
+  }
+  else{
+    return b;
+  }
 }
 function countChar(str, c){
   return str.length, c
