98f36e5d94f57ef3a2574a692296a7dc71ff7eaa
Jason Chay <Jason Chay>
2016-12-10 10:11:21 -0500

    Initialize my repo with my previous homeworks

---
+++ myhomework/hw1.js
@@ -0,0 +1,85 @@
+/*
+Homework 1:
+Solve all TODO assignments
+
+*/
+
+/*
+This is all commented inside the stars
+Blah blah blah
+*/
+
+// double slashes are for single line comments
+
+// TODO: Do the following instructions in your Git Bash - no cheating by using your interface!
+// 1. Create a directory in your desktop named seed-homeworks
+// 2. Inside that directory, create a directory named hw1
+// 3. Go into hw1 and create a file called hw1.js
+// 4. Go back to seed-homeworks and create another directory named tmp
+// 5. Go into tmp and make a file called tmp.txt and another file called tmp2.txt
+// 6. Open up tmp2.txt using Notepad and type in "Goodbye cruel world!".
+// 7. Close Notepad and print out the contents to the screen.
+// 8. Delete tmp.txt
+// 9. Go back to seed-homeworks and remove the tmp directory as a whole
+
+// TODO: Predict the output of the following console log lines.
+// You can uncomment the lines by removing the double slash
+// After uncommenting, you can then run this file to see the output
+
+// Here is an example
+// console.log(5 + 5);
+// 10
+
+// console.log(3 + 2 * 4);
+
+// console.log(10 > 10);
+
+// console.log(1 / 2);
+
+// console.log("Dragon" > "Knight");
+
+// console.log(12 % 7);
+
+// console.log(3 < 3 ? "A" : "B");
+
+// console.log("Bob" + "Mary"); // Is there a space between them or not?
+
+// console.log("Bob","Mary"); // I didn't talk about comma, but you might be able to guess
+
+// console.log("Is this a new
+// 		line");
+
+// console.log("The fox\njumped over the fence");
+
+// console.log(typeof (3 > 2));
+
+// console.log(typeof "Bob");
+
+// console.log(typeof Infinity);
+
+// console.log(typeof NaN);
+
+// console.log(typeof undefined);
+
+// TODO: Using Atom, open hw1.js in the seed-homeworks directory. You can open up the folder in your desktop and right click the file to open with Atom.
+
+// Write from scratch (try not to refer to notes) the output of 17 to the 3rd power.
+
+// Write to the console a message that looks like the following:
+// You have to use '\n'
+// to make a new line
+// NOTE: The above should be shown as two separate lines! Remember how to escape for this!
+
+// OPTIONAL TODO:
+// Create your own Github repository and experiment with Git by yourself!
+// Here are the commands you will need:
+// git status, git clone, git add, git commit, git push, git pull
+
+// Optional TODO:
+// Learn Vim, the editor for professionals :P
+// Use vimtutor or one of the thousands of tutorials online
+// I highly recommend it as an alternative to other text editors
+
+// Optional TODO:
+// Read the first chapter of Eloquent Javascript
+// Online version is free!
