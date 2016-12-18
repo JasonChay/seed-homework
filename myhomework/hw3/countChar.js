d2784e0f14360f075aaa3d5f9d0b447025463cff
Jason Chay <chayjason85@gmail.com>
2016-12-17 21:02:11 -0500

    Uploaded finished countChar function.

--- myhomework/hw3/countChar.js
+++ myhomework/hw3/countChar.js
@@ -1,14 +1,3 @@
-function product(a,b){
-  return a*b;
-}
-function min(a, b){
-  if (a < b){
-    return a;
-  }
-  else{
-    return b;
-  }
-}
 function countChar(str, c){
   var Char = 0;
   for (var i = 0; i <= str.length; i ++){
